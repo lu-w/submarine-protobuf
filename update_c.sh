6e7511f099ee3b92450d237f66dc661e3c6c22c1
lukasw <foxnbk@gmail.com>
2016-03-09 10:46:53 +0100

    Fixed dot.

--- update_c.sh
+++ update_c.sh
@@ -3,4 +3,4 @@
 DST_DIR="../libraries/protocol"
 NANOPB_DIR="../libraries/nanopb"
 
-protoc --plugin=protoc-gen-nanopb=$NANOPB_DIR/generator/protoc-gen-nanopb --nanopb_out=$DST_DIR submarine.proto && sed 's/<pb.h>/"..\/nanopb\/pb.h"/g' $DST_DIR/submarine.pb.h > $DST_DIR/submarine.pb.h.new && rm $DST_DIR/submarine.pb.h && mv $DST_DIR/submarine.pb.h.new .$DST_DIR/submarine.pb.h
+protoc --plugin=protoc-gen-nanopb=$NANOPB_DIR/generator/protoc-gen-nanopb --nanopb_out=$DST_DIR submarine.proto && sed 's/<pb.h>/"..\/nanopb\/pb.h"/g' $DST_DIR/submarine.pb.h > $DST_DIR/submarine.pb.h.new && rm $DST_DIR/submarine.pb.h && mv $DST_DIR/submarine.pb.h.new $DST_DIR/submarine.pb.h
